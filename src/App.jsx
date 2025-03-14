0355319a996894efc478e88822540a502c8ab1fe
Hudiergue <rm95395@estudante.fieb.edu.br>
2025-03-14 09:38:55 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class App extends Component{
+
+  render(){
+
+    return(
+
+      <div> 
+      <h3>Aula de React Vite</h3>
+      <p>Retomada de Conteudo</p>
+      </div>
+
+    )
+  }
+}
+
+export default App
